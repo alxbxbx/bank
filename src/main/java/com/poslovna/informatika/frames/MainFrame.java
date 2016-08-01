662e972137f3bfadbfa4cc00cb367e6fdfad3e76
alxbxbx <acastankovic.bb@gmail.com>
2016-08-01 18:27:43 +0200

    Minor changes

--- src/main/java/com/poslovna/informatika/frames/MainFrame.java
+++ src/main/java/com/poslovna/informatika/frames/MainFrame.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 public class MainFrame extends JFrame {
 
     private JTextField username;
