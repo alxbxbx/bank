479fe2b08746b300a5c24085ec76dcc69170e949
alxbxbx <acastankovic.bb@gmail.com>
2016-08-01 19:10:55 +0200

    Merge

--- src/main/java/com/poslovna/informatika/service/UserService.java
+++ src/main/java/com/poslovna/informatika/service/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.poslovna.informatika.entities.User;
 import com.poslovna.informatika.repository.UserRepository;
-import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
